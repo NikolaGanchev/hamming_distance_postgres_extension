8d1af5d0a0ce6b124871e174d863225163195e2f
NikolaGanchev <nikolamihailovg@gmail.com>
2022-11-17 22:29:19 +0200

    Add hamming distance function

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use pgx::prelude::*;
+
+pgx::pg_module_magic!();
+
+#[pg_extern]
+fn hamming_distance(x: &[u8], y: &[u8]) -> i32 {
+    return i32::try_from(hamming::distance(&x, &y)).unwrap_or_else(|_| -1);
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pg_schema]
+mod tests {
+    use pgx::prelude::*;
+
+    use crate::hamming_distance;
+
+    #[pg_test]
+    fn test_hamming_distance_extension() {
+        assert_eq!(2, hamming_distance(&32_i32.to_ne_bytes(), &45_i32.to_ne_bytes()));
+    }
+
+}
+
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        // return any postgresql.conf settings that are required for your tests
+        vec![]
+    }
+}
